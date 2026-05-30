942c01dcc93d8965805bab4d2681ddd88eb394dc
TLGEC <james.cooling@tlgec.co.uk>
2026-05-30 12:34:20 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='tlgec-survey-v6';
-const FILES=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
+const CACHE='tlgec-survey-v7';
+const FILES=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg','./tlgec-logo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
